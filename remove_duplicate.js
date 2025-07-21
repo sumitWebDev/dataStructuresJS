162ddb5077197b620dc2c5496f0f15f94a46561d
sumitWebDev <dassumit.ece@gmail.com>
2025-07-21 13:04:42 -0600

    Remove element

--- remove_duplicate.js
+++ remove_duplicate.js
@@ -4,10 +4,9 @@ var removeDuplicates = function(nums) {
         if (nums[i-1] !== nums[i] ){
             nums[pointer1+1] = nums[i]
             pointer1++;
-            changesDone++;
         }
     }
-    return nums,pointer1+1
+    return nums
 };
 
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
