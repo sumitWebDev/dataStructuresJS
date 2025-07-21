37725402dfb318f6a0430541cf625b98fa75c13f
sumitWebDev <dassumit.ece@gmail.com>
2025-07-21 09:21:47 -0600

    Remove duplicates

---
+++ remove_duplicate.js
@@ -0,0 +1,13 @@
+var removeDuplicates = function(nums) {
+    let pointer1 = 0;
+    for (let i =1; i< nums.length;i++){
+        if (nums[i-1] !== nums[i] ){
+            nums[pointer1+1] = nums[i]
+            pointer1++;
+            changesDone++;
+        }
+    }
+    return nums,pointer1+1
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
