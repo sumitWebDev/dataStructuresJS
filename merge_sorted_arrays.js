ce61b2fb269da03eb2c82eade48169076ebd91f4
sumitWebDev <dassumit.ece@gmail.com>
2025-07-23 09:22:58 -0600

    Merge sorted Array

--- merge_sorted_arrays.js
+++ merge_sorted_arrays.js
@@ -1,3 +1,5 @@
+
+//Approach 1
 var merge1 = function(nums1, m, nums2, n) {
     nums1Copy = nums1.slice(0,m);
     nums1.length = m+n;
@@ -25,4 +27,23 @@ var merge1 = function(nums1, m, nums2, n) {
     return nums1
 }
 
-console.log(merge1([1],1,[1,2,2],3))
+//Approach 2
+var merge2 = function(nums1, m, nums2, n) {
+    nums1.length = m+n;
+    nums1Pointer = m-1;
+    nums2Pointer = n-1;
+    for(let i =nums1.length-1; i>=0; i--){
+         if((nums2Pointer<0 ) || (nums1[nums1Pointer]>nums2[nums2Pointer] && nums1Pointer>=0)){
+            nums1[i] = nums1[nums1Pointer];
+            nums1Pointer--;
+        }
+        else{
+            nums1[i] = nums2[nums2Pointer]
+            nums2Pointer--;
+        }
+    }
+
+    return nums1
+}
+
+console.log(merge2([2,0],1,[1],1))
