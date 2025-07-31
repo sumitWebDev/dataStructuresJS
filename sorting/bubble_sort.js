046e44c95e28748f5928b313907cf81eb9bce3a7
sumitWebDev <dassumit.ece@gmail.com>
2025-07-30 19:42:56 -0600

    Bubble sorting Modification

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -1,12 +1,15 @@
 function bubble_sort(arr){
     let n = arr.length;
+    let isSwapped = false;
     for(let i =0; i<n-1;i++){
         for (let j = 0; j<n-1-i;j++){
             if(arr[j] > arr[j+1]){
                 arr[j] = arr[j]^arr[j+1];
                 arr[j+1] = arr[j]^arr[j+1]; 
                 arr[j] = arr[j]^arr[j+1]; 
+                isSwapped = true;
             }
+            if (!isSwapped) break; // Important modifcation for Best Case
         }
     }
     return arr
