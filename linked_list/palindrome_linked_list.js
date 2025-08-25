5c73ae80679ae3957942adf95393b71f0925a614
sumitWebDev <dassumit.ece@gmail.com>
2025-08-24 20:28:51 -0500

    Palindrome Linked List

--- linked_list/palindrome_linked_list.js
+++ linked_list/palindrome_linked_list.js
@@ -1,5 +1,7 @@
 //https://leetcode.com/problems/palindrome-linked-list/description/
 
+// Approach 1 - Convert Linked List into Array and then check Palindrome
+// Time complexity - O(n), Space Complexity - O(n)
 var isPalindrome = function(head) {
     
     let arr=[];
@@ -18,16 +20,50 @@ var isPalindrome = function(head) {
     return true
 };
 
-console.log(isPalindrome(({
+
+var isPalindrome2 = function(head) {
+
+    //Finding the middle element
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    // Reverse the second half
+    let prev = null;
+    let current = slow;
+    let temp;
+    while(current){
+        temp = current.next;
+        current.next = prev;
+        prev=current;
+        current = temp;
+    }
+    //Checking for Palindrome
+    let firstList = head;
+    let secondList = prev;
+    
+    while (secondList){
+        if(firstList.val != secondList.val){
+            return false
+        }
+        firstList = firstList.next;
+        secondList = secondList.next;
+    }
+   return true
+};
+
+console.log(isPalindrome2(({
     val: 1,
     next: {
       val: 2,
       next: {
-        val: 3,
+        val: 4,
         next:{
             val: 2,
             next: {
-                val: 1,
+                val: 3,
                 next: null
             }
         }
